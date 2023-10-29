9ea1829d51f22746927c94cb4eacb26ec3bdcaab
yvarsh55 <104003929+yvarsh55@users.noreply.github.com>
2023-10-29 10:27:27 +0530

    Update streamlit_web.py

--- streamlit_web.py
+++ streamlit_web.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pickle
 import pandas as pd 
 import numpy as np 
-import difflib
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
 
 
